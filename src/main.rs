695106833bb83917bd0feed27722d89dac81d4bd
brandon phelps <bwp44f@mst.edu>
2020-10-23 03:36:51 -0500

    added in file opening with some silly error handling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 mod svn_source_funcs;
 
 use std::fs::File;
+use std::path::Path;
+use std::path::PathBuf;
 use std::io::prelude::*;
 
 trait Show {
@@ -48,6 +50,32 @@ impl LocalFileSVNServer {
             root_path: root_path.to_string()
         }
     }
+
+    fn construct_path(path: String, rev: u64) -> PathBuf {
+        PathBuf::new()
+    }
+
+    fn cat_k(&self, path: String, rev: u64) -> Result<String, String> {
+        let p = PathBuf::from(path);
+        // have to clone because String doesn't implement copy so rust would try to move it.
+        // so make full new string and move that in. 
+        let mut k = PathBuf::from(self.root_path.clone());
+        k.push(rev.to_string());
+        k.push(p);
+
+        println!("Looking at: {}", k.display());
+        
+        let path_string = k.as_path().display().to_string();
+
+        let file = File::open(k);
+        match file {
+            Ok(k)  => println!("Succesfully opened file"),
+            Err(_) => return Err(format!("FileRead error: {}", path_string)),
+        }
+        Ok("Hello world".to_string())
+    }
+
+
 }
 
 impl SVNSource for MockSVNServer {
@@ -73,7 +101,19 @@ impl SVNSource for LocalFileSVNServer {
     }
 
     fn cat_r(&self, path: String, rev: u64) -> Result<String, &'static str> {
-        
+        let p = PathBuf::from(path);
+        // have to clone because String doesn't implement copy so rust would try to move it.
+        // so make full new string and move that in. 
+        let mut k = PathBuf::from(self.root_path.clone());
+        k.push(p);
+        k.push(rev.to_string());
+
+        println!("Looking at: {}", k.display());
+        let file = File::open(k);
+        match file {
+            Ok(k)  => println!("Succesfully opened file"),
+            Err(_) => return Err("FileRead error: "),
+        }
         Ok("Hello world".to_string())
     }
 
@@ -81,6 +121,7 @@ impl SVNSource for LocalFileSVNServer {
     fn cat_s(&self) -> &'static str {
         "Hello World"
     }
+
 }
 
 
@@ -101,15 +142,25 @@ fn main() {
     }
 
     let m = MockSVNServer();
-    let l = LocalFileSVNServer::new(r"C:\Users\Brandon\Desktop\rust");
+    let l = LocalFileSVNServer::new(r"C:\Users\Brandon\Desktop\rust\testing");
     println!("cat: {}", m.cat("hello world".to_string()));
     println!("cat: {}", m.cat_s());
 
-    let par =  m.cat_r("testing".to_string(), 1);
+    
 
-
-    match par {
-        Ok(v) => println!("Working with blah {:?}", v),
-        Err(e) => println!("Error: {:?}", e),
+    match l.cat_k("hello.txt".to_string(), 1) {
+        Ok(_) => println!("Unlock"),
+        Err(e) => println!("Reading file error: {}", e),
     }
+
+    // match l.root_path.into_os_string() {
+    //     Ok(k) => println!(k.into_string()),
+    //     Err(_) => println!("Error printing string fkcsl"),
+    // }
+    // println!("ROOT PATH: {}", l.root_path.into_os_string().into_string());
+
+    // match par {
+    //     Ok(v) => println!("Working with blah {:?}", v),
+    //     Err(e) => println!("Error: {:?}", e),
+    // }
 }
